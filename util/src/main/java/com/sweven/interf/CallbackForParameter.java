98203362750dd3c29c1c35ab2ce210e8a6c7fc1a
sweven <sweventears@foxmail.com>
2019-10-17 23:39:04 +0800

    添加带参的回调接口

---
+++ util/src/main/java/com/sweven/interf/CallbackForParameter.java
@@ -0,0 +1,9 @@
+package com.sweven.interf;
+
+/**
+ * Created by Sweven on 2019/10/17--23:01.
+ * Email: sweventears@foxmail.com
+ */
+public interface CallbackForParameter<T> {
+    void call(T t);
+}
