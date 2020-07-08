234e204d3c8cedd16a8c494db849cc29c13a6dee
sweven <sweventears@163.com>
2020-07-08 16:48:07 +0800

    App.main()

--- util/src/main/java/com/sweven/App.kt
+++ util/src/main/java/com/sweven/App.kt
@@ -1,6 +1,9 @@
-package com.sweven.socket
+package com.sweven
+
+import com.sweven.socket.ServiceManager
 
 /**
+ * 测试用例
  * <p>Create by Sweven on 2020/7/3 -- 17:25</p>
  * Email: sweventears@163.com
  */
