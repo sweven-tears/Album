0948e809ffd57534fde3761314c9265e607823d1
sweven <sweventears@163.com>
2020-07-09 16:17:14 +0800

    不应该return，参数不一定只有一个

--- util/src/main/java/com/sweven/ioc/Inject.java
+++ util/src/main/java/com/sweven/ioc/Inject.java
@@ -43,7 +43,6 @@ public class Inject {
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
-                return;
             }
         }
     }
